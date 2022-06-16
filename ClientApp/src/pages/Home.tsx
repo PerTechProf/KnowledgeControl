e6fd60754423669a9afc46fbf0c784022db7b500
deustenebrarum <gamesevolutionteam@gmail.com>
2022-06-16 07:48:03 +0300

    feat: Conditional home

--- ClientApp/src/pages/Home.tsx
+++ ClientApp/src/pages/Home.tsx
@@ -1,5 +1,12 @@
-import { Container } from "react-bootstrap"
+import {Container} from "react-bootstrap"
+import {observer} from "mobx-react-lite";
+import authStore from "../stores/AuthStore";
 
-export const Home = () => {
-  return <Container><h1>Приложение для контроля знаний сотрудников</h1></Container>
-}
+export const Home = observer(() => {
+  return <Container>
+    <h1>
+      {!authStore.token && "Приложение для контроля знаний сотрудников"}
+      {"Вы авторизированы как " + authStore.isEmployer ? "руководитель" : "сотрудник"}
+    </h1>
+  </Container>
+})
