41e30d3fcf6620fb1e2bf05e8ab1846fc7a4560a
deustenebrarum <gamesevolutionteam@gmail.com>
2022-06-16 07:49:51 +0300

    fix: Home doesnt show properly

--- ClientApp/src/pages/Home.tsx
+++ ClientApp/src/pages/Home.tsx
@@ -5,8 +5,8 @@ import authStore from "../stores/AuthStore";
 export const Home = observer(() => {
   return <Container>
     <h1>
-      {!authStore.token && "Приложение для контроля знаний сотрудников"}
-      {"Вы авторизированы как " + authStore.isEmployer ? "руководитель" : "сотрудник"}
+      {!authStore.token ? "Приложение для контроля знаний сотрудников" :
+      `Вы авторизированы как ${authStore.isEmployer ? "руководитель" : "сотрудник"}`}
     </h1>
   </Container>
 })
